4fe985e589df4b5ef87ce2a5c1523caa3250e72c
cristhian <cristhian9bravo@gmail.com>
2020-03-30 14:48:17 -0500

    (1) feat: add currency fields based on workbench

--- currency/models.py
+++ currency/models.py
@@ -4,8 +4,11 @@ from model_utils.models import TimeStampedModel
 
 class Currency(TimeStampedModel):
     name = models.CharField(max_length=255)
+    abbreviation = models.CharField(max_length=45, blank=True, null=True)
     currency_symbol = models.CharField(max_length=3, blank=True)
-
+    currency_format = models.CharField(max_length=40, blank=True, null=True)
+    number_format = models.CharField(max_length=40, blank=True, null=True)
+    
     class Meta:
         db_table = "currency"
         verbose_name_plural = "currencies"

09a119d5c9ac4d5d471fe3eb77654d85a68f2866
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2020-04-02 14:41:08 -0500

    (0.2.1) fix: add post migrate method to populate data

--- country/apps.py
+++ country/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+from django.core.management import call_command
+from django.db.models.signals import post_migrate
+
+
+def populate_data(sender, **kwargs):
+    call_command("loaddata", "country", verbosity=1)
 
 
 class CountryConfig(AppConfig):
     name = 'country'
+
+    def ready(self):
+        post_migrate.connect(populate_data, sender=self)
